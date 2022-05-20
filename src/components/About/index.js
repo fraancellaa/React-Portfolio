0a38ba15e1689c7c7840354c301241274ba85685
Francella Aguilar <francellaaguilar@Francellas-MBP.attlocal.net>
2022-05-20 15:45:37 -0400

    adding about page

---
+++ src/components/About/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import profileImage from "../../assets/profile-image.jpg";
+
+function About() {
+    return (
+        <div className='container'>
+            <div className='row'>
+                <div className='col-6 mt-5 pt-5 offset-md-1'>
+                    <p>Hello, I'm Francella! I am a full stack web developer. I have knowledge of front-end and back-end programming.
+                        <p>I enjoy learning new technologies as I adapt easily to new software.</p>
+                    </p>
+                </div>
+                <div className='col-4 mt-5 pt-5 offset-md-1'>
+                    <img src={profileImage} className="img-fluid" />
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default About;
